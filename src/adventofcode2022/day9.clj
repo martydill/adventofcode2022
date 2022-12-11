248554c4993443e9cc4d5ff75ff650e5fd4610f5
Marty Dill <martydill@users.noreply.github.com>
2022-12-10 19:24:25 -0800

    Day 9 work in progress

--- src/adventofcode2022/day9.clj
+++ src/adventofcode2022/day9.clj
@@ -9,10 +9,60 @@
 (defn number [line]
   (Integer/parseInt (last (str/split line #" "))))
 
+(def visited-positions (atom []))
+
+(defn add-position [pos]
+  (swap! visited-positions (fn [x] (conj @visited-positions pos))))
+
+(defn move-tail [state]
+  (let [hx (first (first state))
+        hy (last (first state))
+        tx (first (last state))
+        ty (last (last state))]
+    (cond
+        ; head 2 squares right, move right
+      (and (= hy ty) (= hx (+ 2 tx))) [[hx hy] [(inc tx) ty]]
+        ; head 2 squares left, move left
+      (and (= hy ty) (= hx (- tx 2))) [[hx hy] [(dec tx) ty]]
+        ; head 2 squares up, move up
+      (and (= hx tx) (= hy (- ty 2))) [[hx hy] [tx (inc ty)]]
+        ; head 2 squares down, move down
+      (and (= hx tx) (= hy (+ 2 ty))) [[hx hy] [tx (dec ty)]]
+
+      ; one square away, dont' need to move
+      (and (<= (abs (- hx tx)) 1) (<= (abs (- hy ty)) 1)) state
+
+      ; same square, don't need to move
+      (and (= hx tx) (= hy ty)) state
+
+         ; head up right diagonal
+      (and (> hy ty) (> hx tx)) [[hx hy] [(inc tx) (inc ty)]]
+        ; head up left diagnoanl
+      (and (> hy ty) (< hx tx)) [[hx hy] [(dec tx) (inc ty)]]
+        ; head down right diagnoal
+      (and (< hy ty) (> hx tx)) [[hx hy] [(inc tx) (dec ty)]]
+        ; head down left diagonal
+      (and (< hy ty) (< hx tx)) [[hx hy] [(dec tx) (dec ty)]]
+
+    ; Don't need to move tail
+      :else (println hx hy tx ty))))
+
+(defn move-head [state x-modifier y-modifier num]
+  (if (= num 0) state
+      (let [cur-x (first (first state))
+            cur-y (last (first state))
+            next-state [[(x-modifier cur-x) (y-modifier cur-y)] [(first (last state)) (last (last state))]]
+            next-state-after-tail-move (move-tail next-state)]
+        (add-position (last next-state-after-tail-move))
+        (recur next-state-after-tail-move x-modifier y-modifier (dec num)))))
+
+(def move-funcs {\U [identity inc] \D [identity dec] \L [dec identity] \R [inc identity]})
+
 (defn process-line [line state]
-(let [dir (diretion line)
-      num (number line))
-(cond (= dir \U) move-)
+  (let [dir (direction line)
+        num (number line)
+        move-funcs (get move-funcs dir)]
+    (move-head state (first move-funcs) (last move-funcs) num)))
 
 (defn run [state lines]
   (if (empty? lines) state
@@ -20,10 +70,7 @@
 
 
 (def start [[100 100] [100 100]])
-(def visited-cells [])
-
-(def unique-visited-cells (set visited-cells))
 
 (run start input)
-  
-  (first "abc")
+(count (set @visited-positions))
+
