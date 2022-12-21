3ec5a1052b8cb9c054006bf7288d17b6fb3e97ab
Marty Dill <martydill@users.noreply.github.com>
2022-12-20 20:14:50 -0800

    Fix part 2 by doing mod least common multiple

--- src/adventofcode2022/day11.clj
+++ src/adventofcode2022/day11.clj
@@ -15,39 +15,46 @@
                    :if_false 6
                    :inspect_count 0},
 
-                  {:items [79,60,97]
-                   :op (fn [old] (* old old))
-                   :test 13
-                   :if_true 1
-                   :if_false 3
-                   :inspect_count 0},
-
-                  {:items [79,60,97]
-                   :op (fn [old] (* old old))
-                   :test 13
-                   :if_true 1
-                   :if_false 3
-                   :inspect_count 0},
-
-                  {:items [79,60,97]
-                   :op (fn [old] (* old old))
-                   :test 13
-                   :if_true 1
-                   :if_false 3
-                   :inspect_count 0},
-
-                  {:items [79,60,97]
-                   :op (fn [old] (* old old))
-                   :test 13
-                   :if_true 1
-                   :if_false 3
-                   :inspect_count 0},
-
-                  {:items [74]
-                   :op (fn [old] (+ old 3))
-                   :test 17
-                   :if_true 0
+                  {:items [50, 74, 67, 92, 61, 76]
+                   :op (fn [old] (* old 13))
+                   :test 19
+                   :if_true 3
                    :if_false 1
+                   :inspect_count 0},
+
+                  {:items [76, 92]
+                   :op (fn [old] (+ old 6))
+                   :test 5
+                   :if_true 1
+                   :if_false 6
+                   :inspect_count 0},
+
+                  {:items [74, 94, 55, 87, 62]
+                   :op (fn [old] (+ old 5))
+                   :test 2
+                   :if_true 2
+                   :if_false 0
+                   :inspect_count 0},
+
+                  {:items [59, 62, 53, 62]
+                   :op (fn [old] (* old old))
+                   :test 7
+                   :if_true 4
+                   :if_false 7
+                   :inspect_count 0},
+
+                  {:items [62]
+                   :op (fn [old] (+ old 2))
+                   :test 17
+                   :if_true 5
+                   :if_false 7
+                   :inspect_count 0},
+
+                  {:items [85, 54, 53]
+                   :op (fn [old] (+ old 3))
+                   :test 13
+                   :if_true 4
+                   :if_false 0
                    :inspect_count 0}])
 
 
@@ -59,7 +66,7 @@
         if-true (:if_true monkey)
         if-false (:if_false monkey)]
     (doseq [item @items]
-      (let [new-worry-level (int (Math/floor (/ (op item) 3)))
+      (let [new-worry-level (int (Math/floor (mod (op item) 9699690)))
             target-monkey-index (if (= (rem new-worry-level test) 0) if-true if-false)
             target-monkey-items (:items (nth @mutable-monkeys target-monkey-index))]
         (reset! mutable-monkeys (assoc-in @mutable-monkeys [target-monkey-index :items] (conj target-monkey-items new-worry-level)))))
@@ -77,7 +84,7 @@
     monkeys
     (recur (dec count) (turn monkeys))))
 
-(reduce * (take 2 (reverse (sort (map :inspect_count (run-until 20 all-monkeys))))))
+(reduce * (take 2 (reverse (sort (map :inspect_count (run-until 10000 all-monkeys))))))
 
 
 
